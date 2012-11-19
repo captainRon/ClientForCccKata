d5f342be4f241fb02fe1eaf5a56407e4e8eefa5d
Sebastian Heglmeier <sebastian.heglmeier@improuv.com>
2012-11-19 21:08:48 +0100

    added dummy test

---
+++ src/test/java/com/candlelightdoener/xpdaysmathudpclient/client/ClientTest.java
@@ -0,0 +1,17 @@
+package com.candlelightdoener.xpdaysmathudpclient.client;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Seb 19.11.12, 21:06
+ */
+public class ClientTest {
+
+    @Test
+    public void testTheTruth() {
+        assertThat(true, is(true));
+    }
+}
